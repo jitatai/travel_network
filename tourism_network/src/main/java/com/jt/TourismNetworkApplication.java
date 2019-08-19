81d91e0da7f783c938aac1f693a6fd53dabc3bf3
佳太 <846722168@qq.com>
2019-08-19 11:11:47 +0800

    将springboot搭建好的项目提交到本地

---
+++ tourism_network/src/main/java/com/jt/TourismNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.jt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourismNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourismNetworkApplication.class, args);
+    }
+
+}
